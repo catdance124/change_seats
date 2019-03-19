0e39adf6bc7b70cd9512ed2892ebaad9a48a6d32
t.kinoshita <t.kinoshita-m@mediaeng.ics.kagoshima-u.ac.jp>
2019-03-19 15:27:32 +0900

    ??

--- js/draggable.js
+++ js/draggable.js
@@ -4,7 +4,6 @@ $(function () {
 		containment: ".field", //ドラッグの範囲を制限
 		snap: ".snaptarget", /* ターゲットにスナップする */
 		snapMode: "inner",   /* 内側にスナップする */
-		// revert: "invalid" /* Droppableな要素以外にDragされた場合、元の位置に戻る */
 	});
 
 $(".snaptarget").droppable({
